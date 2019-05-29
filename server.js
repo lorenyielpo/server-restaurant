6deff495cd70b8b135939b2c529262157772681d
Loreny Ielpo <43501554+lorenyielpo@users.noreply.github.com>
2019-05-28 22:13:08 -0300

    If/else, nodemon

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const http = require('http')
+const http = require("http")
+
+const servidor = http.createServer(function (request, response) {
+    if (request.url === "/") {
+        response.end("Hello world!")
+    } else if (request.url === "/comidas") {
+        if (request.method === "GET") {
+            response.writeHead(200, {
+                "Content-Type":"text/html;charset=utf-8"
+            })
+            response.end("<h1>Resposta do GET</h1>")
+        } else if(request.method === "POST"){
+            response.writeHead(201, {
+                "Content-Type":"text/html;charset=utf-8"
+            })
+            response.end("<h1>Resposta do POST</h1>")
+        }
+    }
+})
+
+servidor.listen(3000)
+console.log("servidor rodando na porta 3000")
