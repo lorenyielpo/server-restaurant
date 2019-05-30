17eac7a62605aa92915be332cd89530217e88cf2
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-29 22:29:13 -0300

    instalação do express

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require("http")
+const express = require("express")
 
 const comidas = {
     comidas: [{
@@ -24,24 +24,10 @@ const comidas = {
     ]
 }
 
-const servidor = http.createServer(function (request, response) {
-    if (request.url === "/") {
-        response.end("Hello world!")
-    } else if (request.url === "/comidas") {
-        if (request.method === "GET") {
-            response.writeHead(200, {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-            })
-            response.write(JSON.stringify(comidas))
-            response.end()
-        } else if (request.method === "POST") {
-            response.writeHead(201, {
-                "Content-Type": "application/json"
-            })
-            response.end("<h1>Resposta do POST</h1>")
-        }
-    }
+const servidor = express()
+
+servidor.get("comidas", (request, response)=>{
+    response.send("Boa noite, amigas!")
 })
 
 servidor.listen(3000)
