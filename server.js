eabbd7663dd4439c286d7a6b2fec04fd61b7e55e
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-30 22:27:28 -0300

    Criação do metodo POST, instalação do body-parser

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
+const bodyParser = require("body-parser")
 const controller = require("./ComidasController")
+
 
 
 const servidor = express()
@@ -8,6 +10,11 @@ servidor.use(cors())
 
 servidor.get("/comidas", (request, response)=>{
     response.send(controller.getAll())
+})
+
+servidor.post("/comidas", bodyParser.json(), (request, response)=>{
+    controller.add(request.body)
+    response.send(201)
 })
 
 servidor.listen(3000)
